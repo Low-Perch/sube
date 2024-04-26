3a845691ad9dea6dd55f1a1be896a195afffa421
cdrani <charlesdrani@gmail.com>
2024-04-26 13:35:33 -0600

    feat: stub panel view

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,25 @@
 <script lang="ts">
+    type Site = { id: string, url: string }
+    let list: Site[] = [
+        {
+            id: "gmail",
+            url: 'https://icons.duckduckgo.com/ip3/mail.google.com.ico'
+        },
+        {
+
+            id: "github",
+            url: 'https://icons.duckduckgo.com/ip3/github.com.ico'
+        },
+    ]
 </script>
 
-<main></main>
+<main>
+    <ul>
+        {#each list as item (item.id)}
+            <li>
+                <span>{item.id}</span>
+                <img src={item.url} />
+            </li>
+        {/each}
+    </ul>
+</main>
