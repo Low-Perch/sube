5b998479f0395f129f3a394328823be8eb5f224b
cdrani <charlesdrani@gmail.com>
2024-04-26 12:11:01 -0600

    refactor: extract setup function into own module

---
+++ src-tauri/src/init.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use tauri::{LogicalPosition, LogicalSize, WebviewUrl};
+
+pub fn setup<'a>(app: &'a mut tauri::App) -> Result<(), Box<dyn Error>> {
+    let width = 1200.;
+    let height = 800.;
+    let window = tauri::window::WindowBuilder::new(app, "main")
+    .inner_size(width, height)
+    .min_inner_size(1200., 800.)
+    .build()?;
+
+    let _webview1 = window.add_child(
+        tauri::webview::WebviewBuilder::new("panel", WebviewUrl::App(Default::default()))
+          .auto_resize(),
+        LogicalPosition::new(0., 0.),
+        LogicalSize::new(50., height),
+    )?;
+
+    let _webview2 = window.add_child(
+        tauri::webview::WebviewBuilder::new(
+          "portal",
+          WebviewUrl::External("https://github.com".parse().unwrap()),
+        )
+        .auto_resize(),
+        LogicalPosition::new(50., 0.),
+        LogicalSize::new(width - 50., height),
+    )?;
+
+    Ok(())
+}
