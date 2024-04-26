5a73a5b2ca9099c11441e598d7c09e0a8c93d617
cdrani <charlesdrani@gmail.com>
2024-04-26 14:23:41 -0600

    feat: display hardcoded panel ui with icons

--- src-tauri/src/init.rs
+++ src-tauri/src/init.rs
@@ -13,7 +13,7 @@ pub fn setup<'a>(app: &'a mut tauri::App) -> Result<(), Box<dyn Error>> {
         tauri::webview::WebviewBuilder::new("panel", WebviewUrl::App(Default::default()))
           .auto_resize(),
         LogicalPosition::new(0., 0.),
-        LogicalSize::new(50., height),
+        LogicalSize::new(25., height),
     )?;
 
     let _webview2 = window.add_child(
@@ -22,8 +22,8 @@ pub fn setup<'a>(app: &'a mut tauri::App) -> Result<(), Box<dyn Error>> {
           WebviewUrl::External("https://github.com".parse().unwrap()),
         )
         .auto_resize(),
-        LogicalPosition::new(50., 0.),
-        LogicalSize::new(width - 50., height),
+        LogicalPosition::new(25., 0.),
+        LogicalSize::new(width - 25., height),
     )?;
 
     Ok(())
--- src/App.svelte
+++ src/App.svelte
@@ -11,14 +11,20 @@
             url: 'https://icons.duckduckgo.com/ip3/github.com.ico'
         },
     ]
+
+    let active = "github"
 </script>
 
-<main>
-    <ul>
+<main class="flex w-full justify-center h-screen bg-black">
+    <ul class="relative grid grid-flow-row place-content-start gap-y-4 py-4">
         {#each list as item (item.id)}
-            <li>
-                <span>{item.id}</span>
-                <img src={item.url} />
+            <li id={item.id} class="relative inline-flex items-center justify-center border-1 rounded border-sno rounded-4 w-6 h-6">
+                <hr 
+                    class:hidden={item.id != active}
+                    class="absolute -left-3 bg-slate-300 h-0.5 w-6 border-none rotate-90"
+                />
+                <img class="w-4 h-4 invert" src={item.url} />
+                <label for={item.id} class="text-xs hidden">{item.id}</label>
             </li>
         {/each}
     </ul>
