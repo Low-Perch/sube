ba5f631b9fec1eca0cfba0f995945f1bac5e0ca8
cdrani <charlesdrani@gmail.com>
2024-04-28 16:23:08 -0600

    feat: connect window events to app builder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod init;
 mod cmd;
+mod init;
 
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .setup(init::setup)
-        .invoke_handler(tauri::generate_handler![
-            cmd::set_webview_url,
-        ])
+        .on_window_event(init::window_events)
+        .invoke_handler(tauri::generate_handler![cmd::set_webview_url,])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
