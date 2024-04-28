f618d7f4ea6347e968990a1194747e44b3ed3cfb
cdrani <charlesdrani@gmail.com>
2024-04-28 16:23:56 -0600

    refactor: setup window and webview creation

--- src-tauri/src/init.rs
+++ src-tauri/src/init.rs
@@ -1,31 +1,30 @@
 use std::error::Error;
-use tauri::{LogicalPosition, LogicalSize, WebviewUrl};
+use tauri::{App, LogicalPosition, LogicalSize, WebviewUrl, Window, WindowEvent, Manager, Webview};
 
-pub fn setup<'a>(app: &'a mut tauri::App) -> Result<(), Box<dyn Error>> {
-    let width = 1200.;
-    let height = 800.;
-    let window = tauri::window::WindowBuilder::new(app, "main")
-    .inner_size(width, height)
-    .min_inner_size(1200., 800.)
-    .build()?;
+const WIDTH: f64 = 900.;
+const HEIGHT: f64 = 600.;
+const PANEL_SIZE_POSITION: f64 = 36.;
 
-    let _webview1 = window.add_child(
-        tauri::webview::WebviewBuilder::new("panel", WebviewUrl::App(Default::default()))
-          .auto_resize(),
-        LogicalPosition::new(0., 0.),
-        LogicalSize::new(36., height),
-    )?;
+fn create_window(app: &App) -> Result<Window, tauri::Error> {
+    tauri::window::WindowBuilder::new(app, "main")
+        .inner_size(WIDTH, HEIGHT)
+        .min_inner_size(WIDTH, HEIGHT)
+        .build()
+}
 
-    let _webview2 = window.add_child(
-        tauri::webview::WebviewBuilder::new(
-          "portal",
-          WebviewUrl::External("https://github.com".parse().unwrap()),
-        )
-        .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15")
-        .auto_resize(),
-        LogicalPosition::new(36., 0.),
-        LogicalSize::new(width - 36., height),
-    )?;
+fn create_webview(window: &Window, label: &str, start_position: f64, width: f64) -> Result<Webview, tauri::Error> {
+    window.add_child(
+        tauri::webview::WebviewBuilder::new(label, WebviewUrl::App(Default::default()))
+        .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"),
+        LogicalPosition::new(start_position, 0.),
+        LogicalSize::new(width, HEIGHT),
+    )
+}
+
+pub fn setup(app: &mut App) -> Result<(), Box<dyn Error>> {
+    let window = create_window(app)?;
+    create_webview(&window, "panel", 0., PANEL_SIZE_POSITION)?;
+    create_webview(&window, "portal", 36., WIDTH - PANEL_SIZE_POSITION)?;
 
     Ok(())
 }
