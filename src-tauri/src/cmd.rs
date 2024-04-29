2fe3cba5d48351fd41590bee65a5105df5ee6098
cdrani <charlesdrani@gmail.com>
2024-04-28 20:24:12 -0600

    refactor: update create_webview args for position

--- src-tauri/src/cmd.rs
+++ src-tauri/src/cmd.rs
@@ -1,10 +1,20 @@
-use tauri::{Manager, Url, Window};
+use tauri::{Manager, Url, Window, LogicalSize, Error as TauriError};
+
+use crate::init::{PANEL_SIZE_POSITION, create_webview};
 
 #[tauri::command]
-pub async fn set_webview_url(window: Window, url: String) -> Result<(), ()> {
+pub async fn set_webview_url(window: Window, url: String) -> Result<(), TauriError> {
     let mut portal = window.get_webview("portal").unwrap();
-    let portal_url = Url::parse(&url).unwrap();
-    portal.navigate(portal_url.clone());
+    if url.starts_with("https") {
+        let portal_url = Url::parse(&url).unwrap();
+        portal.navigate(portal_url);
+    } else {
+        portal.close()?;
+        let window_size = window.inner_size()?;
+        let width = window_size.width as f64;
+        let height = window_size.height as f64;
+        create_webview(&window, "portal", PANEL_SIZE_POSITION, LogicalSize::new(width - PANEL_SIZE_POSITION, height))?;
+    }
 
     Ok(())
 }
