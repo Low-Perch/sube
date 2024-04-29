6a8ff0336026c18c52dd3102798220e66cbaa236
cdrani <charlesdrani@gmail.com>
2024-04-28 20:29:18 -0600

    feat: allow webview to set local file as url

--- src-tauri/src/init.rs
+++ src-tauri/src/init.rs
@@ -1,9 +1,12 @@
 use std::error::Error;
 use tauri::{App, Error as TauriError, LogicalPosition, LogicalSize, WebviewUrl, Url, Window, WindowEvent, Manager, Webview};
 
-const WIDTH: f64 = 900.;
-const HEIGHT: f64 = 600.;
-const PANEL_SIZE_POSITION: f64 = 36.;
+pub const WIDTH: f64 = 900.;
+pub const HEIGHT: f64 = 600.;
+pub const PANEL_SIZE_POSITION: f64 = 36.;
+
+pub const PANEL: &str = "panel";
+pub const PORTAL: &str = "portal";
 
 fn create_window(app: &App) -> Result<Window, TauriError> {
     tauri::window::WindowBuilder::new(app, "main")
@@ -12,32 +15,34 @@ fn create_window(app: &App) -> Result<Window, TauriError> {
         .build()
 }
 
-fn create_webview(window: &Window, label: &str, start_position: f64, width: f64) -> Result<Webview, TauriError> {
+pub fn create_webview(window: &Window, label: &str, start_position: f64, size: LogicalSize<f64>) -> Result<Webview, TauriError> {
     let url = match label {
         "panel" => WebviewUrl::App(Default::default()),
-        _ => WebviewUrl::External(Url::parse("https://google.com").unwrap())
+        _ => WebviewUrl::App("../../portal.html".into())
     };
+
+    let position = LogicalPosition::new(start_position, 0.);
 
     window.add_child(
         tauri::webview::WebviewBuilder::new(label, url)
             // TODO: need to be able to select user_agent based on OS
         .user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"),
-        LogicalPosition::new(start_position, 0.),
-        LogicalSize::new(width, HEIGHT),
+        position, 
+        size
     )
 }
 
 pub fn setup(app: &mut App) -> Result<(), Box<dyn Error>> {
     let window = create_window(app)?;
-    create_webview(&window, "panel", 0., PANEL_SIZE_POSITION)?;
-    create_webview(&window, "portal", 36., WIDTH - PANEL_SIZE_POSITION)?;
+    create_webview(&window, PANEL, 0., LogicalSize::new(PANEL_SIZE_POSITION, HEIGHT))?;
+    create_webview(&window, PORTAL, PANEL_SIZE_POSITION, LogicalSize::new(WIDTH - PANEL_SIZE_POSITION, HEIGHT))?;
 
     Ok(())
 }
 
 pub fn window_events(window: &Window, event: &WindowEvent) {
-    let panel = window.get_webview("panel").unwrap();
-    let portal = window.get_webview("portal").unwrap();
+    let panel = window.get_webview(PANEL).unwrap();
+    let portal = window.get_webview(PORTAL).unwrap();
 
     match event {
         WindowEvent::Resized(dimensions) => {
