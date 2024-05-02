6e0553b875c3b3b46161a75c3c3a1b50735b24e2
cdrani <charlesdrani@gmail.com>
2024-05-01 23:58:17 -0600

    feat: add search/input bar in title bar

--- src/title_bar/TitleBar.svelte
+++ src/title_bar/TitleBar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts"></script>
 
-<main data-tauri-drag-region class="flex h-9 w-full items-center">
+<main data-tauri-drag-region class="flex h-9 w-full justify-between items-center">
     <div class="flex items-center relative left-9 w-24 gap-x-2 h-full place-items-center">
         <button
             on:click={() => {}}
@@ -40,5 +40,9 @@
                 <path class="fill-slate-200" d="M14 8.495v-.5h-1v.5zM7.5 2.999H8v-1h-.5zm1-.5l.353.353l.354-.353l-.354-.354zM13 8.495a5.499 5.499 0 0 1-5.5 5.496v1c3.589 0 6.5-2.909 6.5-6.496zM7.5 13.99A5.499 5.499 0 0 1 2 8.495H1a6.499 6.499 0 0 0 6.5 6.496zM2 8.495a5.499 5.499 0 0 1 5.5-5.496v-1A6.499 6.499 0 0 0 1 8.495zM6.147.854l2 1.998l.706-.707l-2-1.999zm2 1.291l-2 1.999l.706.707l2-1.999z" />
             </svg>
         </button>
+    </div>
+
+    <div class="flex-col justify-center place-content-center items-end w-full h-9 max-w-screen-sm md:max-w-screen-md">
+        <input class="w-full bg-black border-gray-500 border-2 text-gray-300 h-8 my-0.5 px-2 rounded-md" />
     </div>
 </main>
