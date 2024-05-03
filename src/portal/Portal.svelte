2168e5f42ac714b0fe403bf82c4962359adec74e
cdrani <charlesdrani@gmail.com>
2024-05-03 15:56:08 -0600

    refactor: extract SiteGrid component

--- src/portal/Portal.svelte
+++ src/portal/Portal.svelte
@@ -1,18 +1,11 @@
 <script lang="ts">
     import { writable } from 'svelte/store'
-    import { invoke } from '@tauri-apps/api/core'
 
     import Input from './components/Input.svelte'
+    import SiteGrid from './components/SiteGrid.svelte'
     import { sites, type Site } from '../utils/constants'
-    import { emitTo } from '@tauri-apps/api/event'
 
     const favourites = writable<Site[]>(sites)
-
-    async function setActionTab(e: MouseEvent) {
-        const button = e.currentTarget as HTMLButtonElement
-        await emitTo('panel', 'switch_tab', { tab: button.name })
-        await invoke('set_webview_url', { url: button.value })
-    }
 
     function filterList(event: CustomEvent) {
         const searchValue = event.detail.value
@@ -25,32 +18,6 @@
     <div class="flex-col my-10 mx-auto w-full max-w-screen-md">
         <Input on:search={filterList} />
 
-        <h2 class="text-slate-100 text-xl mt-10">Favourites</h2>
-
-        <div class="flex flex-wrap gap-4 my-10 mx-auto">
-            {#each $favourites as site (site.id)}
-                <button
-                    name={site.id}
-                    value={site.url}
-                    on:click={setActionTab}
-                    class="flex flex-col bg-slate-400 rounded-md place-content-center justify-center items-center gap-2 w-20 h-20"
-                >
-                    {#if site.ico.startsWith('https')}
-                        <img
-                            alt={site.id}
-                            class="inline-flex justify-center w-6 h-6 bg-transparent"
-                            src={site.ico}
-                        />
-                    {:else}
-                        <span
-                            class="flex relative -top-3 justify-center item-center w-5 h-5 text-3xl text-white"
-                        >
-                            {@html site.ico}
-                        </span>
-                    {/if}
-                    <label for={site.id} class="text-sm text-slate-100">{site.id}</label>
-                </button>
-            {/each}
-        </div>
+        <SiteGrid sites={$favourites} title="Favourites" />
     </div>
 </main>
