8c161540fc133184694e0bca0d617172ecc23c93
cdrani <charlesdrani@gmail.com>
2024-05-05 20:08:36 -0600

    feat: add util to validate possible web address

---
+++ src/utils/validators.ts
@@ -0,0 +1,5 @@
+export function isValidAddress(url: string) {
+    const regex =
+        /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi
+    return regex.test(url)
+}
