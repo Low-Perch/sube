3c32460f414564ffc7d538b8e14f38658812c1bb
cdrani <charlesdrani@gmail.com>
2024-05-05 22:47:44 -0600

    feat: show search mid-state ui

--- src/portal/Portal.svelte
+++ src/portal/Portal.svelte
@@ -3,14 +3,36 @@
 
     import Input from './components/Input.svelte'
     import SiteGrid from './components/SiteGrid.svelte'
+
+    import { isValidAddress } from '../utils/validators'
     import { sites, type Site } from '../utils/constants'
 
+    const search = writable<string>('')
+    const site = writable<Site | null>(null)
     const favourites = writable<Site[]>(sites)
 
     function filterList(event: CustomEvent) {
         const searchValue = event.detail.value
-        let filteredList = sites.filter(({ id }) => id.startsWith(searchValue))
+        search.set(searchValue)
+        const filteredList = sites.filter(({ id }) => id.startsWith(searchValue))
+
         favourites.set(filteredList)
+        updateSite(searchValue)
+    }
+
+    function updateSite(value: string) {
+        if (!isValidAddress(value)) {
+            return site.set(null)
+        }
+
+        const possibleUrl = value.startsWith('http') ? value : `https://${value}`
+        const url = new URL(possibleUrl)
+
+        site.set({
+            id: url.host,
+            url: url.href,
+            ico: `https://icons.duckduckgo.com/ip3/${url.host}.ico`
+        })
     }
 </script>
 
@@ -18,6 +40,17 @@
     <div class="flex-col my-10 mx-auto w-full max-w-screen-md">
         <Input on:search={filterList} />
 
-        <SiteGrid sites={$favourites} title="Favourites" />
+        {#if !!$favourites.length}
+            <SiteGrid sites={$favourites} title="Favourites" />
+        {:else if $site}
+            <SiteGrid sites={[$site]} title="Found" />
+        {:else}
+            <div
+                class="flex gap-2 text-2xl text-slate-400 justify-center items-center mt-10 h-72 bg-gray-700 rounded-xl"
+            >
+                <span class="relative top-1">{@html '&#128269;'}</span>
+                <p>{$search}</p>
+            </div>
+        {/if}
     </div>
 </main>
