825366e9320c22aea5686939d7c46b7a07afdee6
cdrani <charlesdrani@gmail.com>
2024-05-08 00:08:54 -0600

    chore: remove hard-coded sites list

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,4 @@
-export type Site = {
-    id: string
-    url: string
-    ico: string
-}
+export type Site = { id: string; url: string; ico: string }
 
 export const genSvg = (id: string) => `https://api.iconify.design/simple-icons:${id}.svg`
 
@@ -11,36 +7,3 @@ export const HOME: Site = {
     url: '../../panel.html',
     ico: '&#x2b;'
 }
-
-export const sites: Site[] = [
-    {
-        id: 'google',
-        url: 'https://google.com',
-        ico: genSvg('https://google.com')
-    },
-    {
-        id: 'github',
-        url: 'https://github.com',
-        ico: genSvg('https://github.com')
-    },
-    {
-        id: 'spotify',
-        url: 'https://open.spotify.com',
-        ico: genSvg('https://spotify.com')
-    },
-    {
-        id: 'tauri',
-        url: 'https://beta.tauri.app',
-        ico: genSvg('https://tauri.app')
-    },
-    {
-        id: 'gmail',
-        url: 'https://mail.google.com',
-        ico: genSvg('https://mail.google.com')
-    },
-    {
-        id: 'youtube',
-        url: 'https://youtube.com',
-        ico: genSvg('https://youtube.com')
-    }
-]
