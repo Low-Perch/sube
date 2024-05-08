9b5c8b8af85b4d88a8a9d3a456061f0c64a5a821
cdrani <charlesdrani@gmail.com>
2024-05-07 21:20:39 -0600

    feat: connect personas to panel

--- src/panel/Panel.svelte
+++ src/panel/Panel.svelte
@@ -4,27 +4,35 @@
     import { invoke } from '@tauri-apps/api/core'
     import { listen } from '@tauri-apps/api/event'
 
-    import { sites, type Site } from '../utils/constants'
+    import { genSvg, HOME, type Site } from '../utils/constants'
 
-    let activeTab = writable<Site>(sites.at(0))
+    const activeTab = writable<Site>(HOME)
+    const tabs = writable<Site[]>([])
 
     async function setActionTab(e: MouseEvent) {
         const button = e.currentTarget as HTMLButtonElement
-        const site = sites.find(({ id }) => id == button.name)
+        const site = $tabs.find(({ id }) => id == button.name)
         if (!site) return
 
         activeTab.set(site)
         await invoke('set_webview_url', { url: site.url })
     }
 
+    async function loadPanel() {
+        const { sites } = await invoke('get_persona', { id: null })
+        const list = [HOME, ...sites.map((site) => ({ ...site, ico: genSvg(site.id) }))]
+        tabs.set(list)
+    }
+
     onMount(() => {
         ;(async () => {
-            listen
+            await loadPanel()
+
             const unlisten = await listen<{ tab: string }>('switch_tab', (event) => {
                 const tab = event?.payload?.tab
                 if (!tab) return
 
-                const site = sites.find(({ id }) => id == tab)
+                const site = $tabs.find(({ id }) => id == tab)
                 site && activeTab.set(site)
             })
 
@@ -35,7 +43,7 @@
 
 <main class="fixed h-full w-full justify-center items-center py-2">
     <ul class="relative grid grid-flow-row place-content-start gap-y-2">
-        {#each sites as site (site.id)}
+        {#each $tabs as site (site.id)}
             <li id={site.id} class="relative inline-flex items-center mx-auto w-8 h-8">
                 <hr
                     class:hidden={site.id != $activeTab.id}
@@ -48,11 +56,13 @@
                     class="inline-flex ml-2 justify-center items-center"
                 >
                     {#if site.ico.startsWith('https')}
-                        <img alt={site.id} class="w-5 h-5 bg-transparent" src={site.ico} />
+                        <img alt={site.id} class="invert w-5 h-5 bg-transparent" src={site.ico} />
                     {:else}
-                        <span class="flex relative -top-2.5 item-center w-5 h-5 text-3xl text-white"
-                            >{@html site.ico}</span
+                        <span
+                            class="flex relative -top-2.5 item-center w-5 h-5 text-3xl text-white"
                         >
+                            {@html site.ico}
+                        </span>
                     {/if}
                     <label for={site.id} class="text-xs hidden">{site.id}</label>
                 </button>
