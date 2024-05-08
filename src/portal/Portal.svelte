a90a9ecda1a5924095b8319fd57b8ad477420ced
cdrani <charlesdrani@gmail.com>
2024-05-07 22:29:49 -0600

    feat: connect persona sites to Portal

--- src/portal/Portal.svelte
+++ src/portal/Portal.svelte
@@ -1,49 +1,63 @@
 <script lang="ts">
+    import { onMount } from 'svelte'
     import { writable } from 'svelte/store'
+    import { invoke } from '@tauri-apps/api/core'
 
     import Input from './components/Input.svelte'
     import SiteGrid from './components/SiteGrid.svelte'
 
     import { isValidAddress } from '../utils/validators'
-    import { sites, type Site } from '../utils/constants'
+    import { genSvg, HOME, type Site } from '../utils/constants'
 
     const search = writable<string>('')
-    const site = writable<Site | null>(null)
-    const favourites = writable<Site[]>(sites)
+    const tab = writable<Site | null>(null)
+    const tabs = writable<Site[]>([])
 
     function filterList(event: CustomEvent) {
         const searchValue = event.detail.value
         search.set(searchValue)
-        const filteredList = sites.filter(({ id }) => id.startsWith(searchValue))
+        const filteredList = $tabs.filter(({ id }) => id.startsWith(searchValue))
 
-        favourites.set(filteredList)
+        tabs.set(filteredList)
         updateSite(searchValue)
     }
 
     function updateSite(value: string) {
         if (!isValidAddress(value)) {
-            return site.set(null)
+            return tab.set(null)
         }
 
         const possibleUrl = value.startsWith('http') ? value : `https://${value}`
         const url = new URL(possibleUrl)
 
-        site.set({
+        tab.set({
             id: url.host,
             url: url.href,
             ico: `https://icons.duckduckgo.com/ip3/${url.host}.ico`
         })
     }
+
+    async function loadPanel() {
+        const { sites } = await invoke('get_persona', { id: null })
+        const list = [HOME, ...sites.map((site) => ({ ...site, ico: genSvg(site.id) }))]
+        tabs.set(list)
+    }
+
+    onMount(() => {
+        ;(async () => {
+            await loadPanel()
+        })()
+    })
 </script>
 
 <main class="flex-col h-dvh justify-center mx-auto bg-gray-800 p-20">
     <div class="flex-col my-10 mx-auto w-full max-w-screen-md">
         <Input on:search={filterList} />
 
-        {#if !!$favourites.length}
-            <SiteGrid sites={$favourites} title="Favourites" />
-        {:else if $site}
-            <SiteGrid sites={[$site]} title="Found" />
+        {#if !!$tabs.length}
+            <SiteGrid sites={$tabs} title="Favourites" />
+        {:else if $tab}
+            <SiteGrid sites={[$tab]} title="Found" />
         {:else}
             <div
                 class="flex gap-2 text-2xl text-slate-400 justify-center items-center mt-10 h-72 bg-gray-700 rounded-xl"
