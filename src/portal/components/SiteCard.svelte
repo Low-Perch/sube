5d864101142ff36f2ba490321a29ce38fcc1a596
cdrani <charlesdrani@gmail.com>
2024-05-07 21:16:27 -0600

    fix: revert image styles updates

--- src/portal/components/SiteCard.svelte
+++ src/portal/components/SiteCard.svelte
@@ -20,11 +20,7 @@
     class="flex flex-col bg-slate-400 rounded-md place-content-center justify-center items-center gap-2 min-w-20 min-h-20 aspect-video"
 >
     {#if site.ico.startsWith('https')}
-        <img
-            alt={site.id}
-            style="--src: url({site.ico})"
-            class="image invert inline-flex justify-center w-6 h-6"
-        />
+        <img alt={site.id} src={site.ico} class="inline-flex justify-center w-6 h-6" />
     {:else}
         <span class="flex relative -top-3 justify-center item-center w-5 h-5 text-3xl text-white">
             {@html site.ico}
