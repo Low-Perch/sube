db3cc8e2c50ccba2d73506c79cb62ecee3d57822
cdrani <charlesdrani@gmail.com>
2024-05-09 15:25:49 -0600

    chore: remove sites file

--- src/shared/sites.ts
+++
@@ -1,9 +0,0 @@
-import { invoke } from '@tauri-apps/api/core'
-
-import { genSvg, HOME } from '../utils/constants'
-
-// TODO: get types form rust Persona & Site structs
-export const loadPersonaSites = async () => {
-    const sites = await invoke('get_sites')
-    return [HOME, ...sites.map((site) => ({ ...site, ico: genSvg(site.id) }))]
-}
