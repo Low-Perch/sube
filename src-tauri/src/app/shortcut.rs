46da03f3c61e64ebb376b649559760eda74cfa86
cdrani <charlesdrani@gmail.com>
2024-05-11 18:27:01 -0600

    refactor: update shortcut builder api

--- src-tauri/src/app/shortcut.rs
+++ src-tauri/src/app/shortcut.rs
@@ -1,13 +1,15 @@
 use crate::app::setup::MAIN;
 use tauri::{App, Manager};
-use tauri_plugin_global_shortcut::{Code, Modifiers};
+use tauri_plugin_global_shortcut::{Code, Modifiers, ShortcutState};
 
 pub fn init(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
     app.handle().plugin(
         tauri_plugin_global_shortcut::Builder::new()
             .with_shortcuts(["ctrl+\\"])?
-            .with_handler(|app, shortcut| {
-                if shortcut.matches(Modifiers::CONTROL, Code::Backslash) {
+            .with_handler(|app, shortcut, event| {
+                if event.state == ShortcutState::Pressed
+                    && shortcut.matches(Modifiers::CONTROL, Code::Backslash)
+                {
                     let window = app.get_window(MAIN).unwrap();
 
                     if window.is_visible().unwrap() {
